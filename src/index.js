756200716f1bd8bf4e6df8ac4011da1390203527
MatinT-SA <matin.taherzadeh.mmtsa@gmail.com>
2024-12-24 15:24:19 +0330

    improving reusability by adding more flexibility to our component

--- src/index.js
+++ src/index.js
@@ -1,13 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom/client";
 import StarRating from "./StarRating";
 // import "./index.css";
 // import App from "./App";
 
+function Test() {
+  const [movieRating, setMovieRating] = useState(0);
+
+  return (
+    <div>
+      <StarRating color="red" maxRating={10} onSetRating={setMovieRating} />
+      <p>this movie has the {movieRating} rating</p>
+    </div>
+  );
+}
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     {/* <App /> */}
-    <StarRating maxRating={5} />
+    <StarRating maxRating={5} defaultRating={3} />
+    <StarRating
+      maxRating={5}
+      color="blue"
+      size="30"
+      className="test"
+      messages={["Terrible", "Bad", "Okay", "Good", "Nice"]}
+      defaultRating={3}
+    />
+    <Test />
   </React.StrictMode>
 );
