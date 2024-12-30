ceb7ec9e6c05082fab4c51f92dc5a1e49bbca97a
MatinT-SA <matin.taherzadeh.mmtsa@gmail.com>
2024-12-30 21:44:44 +0330

    implementing deletion logic for watched movies

--- src/components/Main/Watched/WatchedMovie.js
+++ src/components/Main/Watched/WatchedMovie.js
@@ -1,4 +1,4 @@
-export function WatchedMovie({ movie }) {
+export function WatchedMovie({ movie, onDeleteWatch }) {
   return (
     <li>
       <img src={movie.poster} alt={`${movie.title} poster`} />
@@ -16,6 +16,13 @@ export function WatchedMovie({ movie }) {
           <span>⏳</span>
           <span>{movie.runtime} min</span>
         </p>
+
+        <button
+          className="btn-delete"
+          onClick={() => onDeleteWatch(movie.imdbID)}
+        >
+          X
+        </button>
       </div>
     </li>
   );
